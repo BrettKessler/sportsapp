73dea6b64dcdaca3270dbaf8eab76b15e31cb7bb
Brett Kessler <brettkessler1@gmail.com>
2017-02-27 21:27:21 +0000

    make it better

--- app.js
+++ app.js
@@ -74,3 +74,43 @@ function tweeted(error, data, response) {
   }
 
 }}
+
+var express = require("express");
+var app = express();
+var request = require ("request");
+app.set("view engine", "ejs");
+
+// this gets the current date and converts it to numerical form.
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10){
+}
+if(mm<10){
+    mm='0'+mm
+}
+
+// this is getting information from the API and returning results in the ejs.
+
+
+ 
+
+request("https://api.sportradar.us/ncaamb-t3/games/" + yyyy + "/" + mm + "/" + dd + "/schedule.json?api_key=j6mu95u99hsaayj5etfuzh6w", function(error, response, body){
+    if(!error && response.statusCode == 200){
+        var data = JSON.parse(body);
+      data.games.forEach((v,i) => {
+    setTimeout(() => {
+        T.post('statuses/update', { status: v.home.name + " VS " + v.away.name },
+            function(err, data, response) {
+                // ... handle the response as you like
+            });
+    }, i * 1000*60*10);
+});
+
+
+ }
+});   
+
+ 
